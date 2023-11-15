32b02b8674a00b70772ecc630fdc85a0c4cb6971
khaibar28 <muhkhaibar2003@gmail.com>
2023-11-15 13:33:03 +0800

    migration + model

---
+++ app/app/Models/Task.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+    protected $fillable = [
+        'task_description',
+        'deadline',
+        'subjects_id',
+        'classess_id',
+    ];
+
+    protected $dates = ['deadline'];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class, 'subjects_id');
+    }
+
+    public function classes()
+    {
+        return $this->belongsTo(Classes::class, 'classess_id');
+    }
+}
+
+
+
+
+
+
