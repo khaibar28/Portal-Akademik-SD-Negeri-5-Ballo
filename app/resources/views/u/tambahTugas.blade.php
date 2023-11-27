fd92894aa1cfa7426f6489462d2a0db4b339b2ee
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-11-27 19:36:20 +0800

    tambah tugas

--- app/resources/views/u/tambahTugas.blade.php
+++ app/resources/views/u/tambahTugas.blade.php
@@ -38,6 +38,40 @@
             <div class="col-md-3">
                 <input name='deadline' type="date" id="datepicker" class="form-control border-secondary"
                 placeholder="Input Date and Time" aria-describedby="basic-addon1">
+                {{-- <img src="{{ asset('img/date.svg') }}" alt=""> --}}
+            </div>
+
+            <div class="row">
+                <div class="col-md-9"></div>
+                <div class="col-md-1">
+                    <a href="{{ route('tugas') }}" type="button" class="btn btn-light"
+                        style="box-shadow: 0px 4px 4px 0px #00000040; border-radius: 12px; color:#3182FB">Batal</a>
+                </div>
+                <div class="col-md-2">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        style="box-shadow: 0px 4px 4px 0px #00000040; border-radius: 12px">Simpan</button>
+                </div>
+            </div>
+            
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <center>
+                            <div class="mt-3">
+                                <img src="{{ asset('img/alert_biru.svg') }}" alt="">
+                            </div>
+                            <div class="modal-body p-0">
+                                <p class="m-0 mt-1" style="font-weight: bold; font-size: 15px">Are you sure want to saving?</p>
+                                <p class="m-0 mt-1" style="font-size: 12px">Are you sure want to saving?</p>
+                            </div>
+                            <div class="p-3">
+                                <button type="button" class="btn" data-bs-dismiss="modal"
+                                    style="border: 1px #3182FB solid">No</button>
+                                <button type="submit" class="btn btn-primary">Yes</button>
+                            </div>
+                        </center>
+                    </div>
+                </div>
             </div>
 
             <script src="https://cdn.jsdelivr.net/npm/flatpickr">
