5ad621a87925e7172792b0d9873e627f04ba103b
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-11-29 22:30:04 +0800

    fix dropdown

--- app/resources/views/u/editrekap.blade.php
+++ app/resources/views/u/editrekap.blade.php
@@ -6,42 +6,8 @@
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="col-md-3">
-      Tahun Ajaran/Semester<i class="fa-solid fa-asterisk fa-2xs" style="color: #f60000;"></i> 
-      <select class="form-select btn-primary border-secondary" aria-label="Default select example" >
-        <option selected disabled>Pilih Tahun Pelajaran</option>
-        <option value="1">2023/Ganjil</option>
-        <option value="2">2023/Genap</option>
-        <option value="3">2022/Ganjil</option>
-      </select>
-    </div>
+    <div class="col-md-10"></div>
     <div class="col-md-2">
-      Kelas<i class="fa-solid fa-asterisk fa-2xs" style="color: #f60000;"></i>
-      <select class="form-select btn-primary border-secondary" aria-label="Default select example" >
-        <option selected disabled>Pilih Kelas</option>
-        <option value="1">Satu</option>
-        <option value="2">Dua</option>
-        <option value="3">Tiga</option>
-        <option value="4">Empat</option>
-        <option value="5">Lima</option>
-        <option value="6">Enam</option>
-      </select>
-    </div>
-    <div class="col-md-3">
-      Pilih Mata pelajaran<i class="fa-solid fa-asterisk fa-2xs" style="color: #f60000;"></i> 
-      <select class="form-select btn-primary border-secondary" aria-label="Default select example" >
-        <option selected disabled>Pilih Mata Pelajaran</option>
-        <option value="1">Matematika</option>
-        <option value="2">Bhs. Indonesia</option>
-        <option value="3">IPA</option>
-      </select>
-    </div>
-    <div class="col-md-2">
-        <br>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-    <div class="col-md-2">
-      <br>
       <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-warning ms-4" style="box-shadow: 0px 4px 4px 0px #00000040;
             border-radius: 12px">Konfirmasi</button>
     </div>
@@ -76,7 +42,7 @@
   </div> --}}
 
   {{-- kalau sudah ada data --}}
-  <table class="table mt-5">
+  <table class="table mt-3">
     <thead>
         <tr>
             <th scope="col" class="text-center">No.</th>
