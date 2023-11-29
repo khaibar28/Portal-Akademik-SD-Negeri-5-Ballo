da46359de0a11412513f75242006b1484f943536
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2023-11-29 23:15:15 +0800

    tugas

--- app/resources/views/u/tugas.blade.php
+++ app/resources/views/u/tugas.blade.php
@@ -52,23 +52,5 @@
         <img src="{{ asset('img/no_data.svg') }}" alt="" class="mt-4">
     </center>
   </div>
-
-  
-    {{-- <table class="table mt-5">
-        <thead>
-            <tr>
-                <th scope="col" class="text-center">No.</th>
-                <th scope="col" class="text-center">Deskripsi Tugas</th>
-                <th scope="col" class="text-center">Deadline</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row" class="text-center">1</td>
-                <td scope="row">Buku Cetak Matematika Hal. 59</td>
-                <td scope="row" class="text-center">30 Februari 2024</td>
-            </tr>
-        </tbody>
-        </table> --}}
 </div>
 @endsection
