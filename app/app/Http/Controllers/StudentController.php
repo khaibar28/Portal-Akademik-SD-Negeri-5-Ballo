0886133a7ca77755a59c0ea8da67b58581335170
khaibar28 <muhkhaibar2003@gmail.com>
2023-12-04 22:57:38 +0800

    perbaiki read nilai siswa

--- app/app/Http/Controllers/StudentController.php
+++ app/app/Http/Controllers/StudentController.php
@@ -12,35 +12,36 @@ class StudentController extends Controller
 {
     public function rekap(Request $request){
         $dataClass = Score::where('user_id', auth()->user()->id)->distinct()->pluck('classess_id');
-    
+
         if ($request->isMethod('post')) {
-            // Jika request menggunakan metode POST, simpan data ke session
-            $request->session()->put('dataScore', $this->readRekap($request));
-            return redirect()->route('rekap');
-        }
-    
-        $dataScore = Score::where('user_id', auth()->user()->id)->where('classess_id', function ($query) {
-            $query->select('classess_id')
-                ->from('scores')
-                ->where('user_id', auth()->user()->id)
-                ->orderBy('classess_id', 'desc')
-                ->limit(1);
-        })
-        ->get();
-    
-        return view('rekap', compact('dataClass', 'dataScore'));
-    }
-    
-    public function readRekap(Request $request){
-        $grade = $request->input('grade');
-        $dataScore = DB::table('scores')
-            ->join('classess', 'scores.classess_id', '=', 'classess.id')
-            ->where('scores.user_id', auth()->user()->id)
-            ->where('classess.grade', $grade)
+            $grade = $request->input('grade');
+
+            $dataScore = Score::where('user_id', auth()->user()->id)->where('classess_id', function ($query) use($grade) {
+                $query->select('classess_id')
+                    ->from('scores')
+                    ->where('user_id', auth()->user()->id)
+                    ->where('classess_id', $grade)->first();
+
+            })
             ->get();
-        return $dataScore;
+
+                return view('rekap', compact('dataClass', 'dataScore'));
+       }else if ($request->isMethod('get')) {
+                $dataScore = Score::where('user_id', auth()->user()->id)->where('classess_id', function ($query) {
+                    $query->select('classess_id')
+                        ->from('scores')
+                        ->where('user_id', auth()->user()->id)
+                        ->orderBy('classess_id', 'desc')
+                        ->limit(1);
+                })
+                ->get();
+
+                return view('rekap', compact('dataClass', 'dataScore'));
+
+            }
+
+
     }
-    
 
     public function tugas(){
         return view('tugas');
